47acad272685ed0061cd80c57d16c59988370c07
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-01-11 02:57:13 +0100

    Optimizatin code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './../react-pages/Home';
-import About from './../react-pages/About';
+import Home from '../pages/_Home';
+import About from '../pages/_About';
 import TweenLight from './TweenLight';
 
 const App = () => {
