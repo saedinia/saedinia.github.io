41a2d36e0b2b64e24c880edace521bdb51e82e75
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-01-12 11:26:32 +0100

    Fixed image link

--- src/pages/_Home.tsx
+++ src/pages/_Home.tsx
@@ -1,10 +1,9 @@
-import { Link } from 'react-router-dom';
-
 const Home = () => {
   return (
     <div className="container">
       <header>
         <div className="brand"></div>
+        <img src="/images/profile.jpg" alt="" />
         <nav></nav>
       </header>
     </div>
