ca96c492c69ba09de974385c02e45f2a70f670f6
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-10-24 14:53:52 +0200

    Updated resume pdf file

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -83,7 +83,7 @@ function Profile() {
         <div className="divider m-0 bg-primary h-0"></div>
         <div className="links">
           <a
-            href="/pdf/Resume_Sadegh-SaediNia.pdf"
+            href="/pdf/Lebenslauf_Sadegh-SaediNia.pdf"
             className="py-4 bg-primary hover:text-secondary"
           >
             <svg
@@ -100,7 +100,7 @@ function Profile() {
             </svg>
             Download CV
           </a>
-          <div className="divider divider-horizontal w-0 m-0"></div>
+          {/* <div className="divider divider-horizontal w-0 m-0"></div>
           <a href="#contact" className="py-4 bg-primary hover:text-secondary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -111,7 +111,7 @@ function Profile() {
               <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
             </svg>
             Contact me
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
